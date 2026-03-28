161c834c8cdd65e33e333eb0d4966b34eea960a9
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-03-28 16:04:58 -0700

    fix: separate multiple frames in usb transfer

--- app/src/main/java/io/benwiegand/projection/geargrinder/transfer/UsbTransferInterface.java
+++ app/src/main/java/io/benwiegand/projection/geargrinder/transfer/UsbTransferInterface.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import io.benwiegand.projection.geargrinder.message.AAFrame;
+
 // uses USB bulk transfers to separate AA messages
 // other implementations may need to leverage the 16-bit payload size in each message
 public class UsbTransferInterface implements AATransferInterface {
@@ -13,6 +15,8 @@ public class UsbTransferInterface implements AATransferInterface {
     private final InputStream is;
     private final OutputStream os;
     private final int maxTransferSize;
+    private final byte[] readCache;
+    private int readCacheLength = 0;
 
     private boolean alive = true;
 
@@ -21,6 +25,7 @@ public class UsbTransferInterface implements AATransferInterface {
         this.is = is;
         this.os = os;
         this.maxTransferSize = maxTransferSize;
+        readCache = new byte[maxTransferSize];
     }
 
     @Override
@@ -39,11 +44,34 @@ public class UsbTransferInterface implements AATransferInterface {
         }
     }
 
+    private void readTransfer() throws IOException {
+        assert readCacheLength <= 0;
+        int len = is.read(readCache);
+        if (len < 0) throw new IOException("stream closed (" + len + ")");
+
+        readCacheLength = len;
+    }
+
     @Override
     public int readFrame(byte[] buffer) throws IOException {
         try {
-            int len = is.read(buffer);   // one usb bulk transfer per message
-            if (len < 0) throw new IOException("stream closed (" + len + ")");
+            if (readCacheLength <= 0) readTransfer();
+            AAFrame frame = new AAFrame(readCache);
+            int len = frame.getLength();
+
+            if (len > readCacheLength)
+                throw new IOException("frame length out of bounds");
+
+            System.arraycopy(frame.getBuffer(), 0, buffer, 0, len);
+
+            if (frame.getLength() == readCacheLength) {
+                readCacheLength = 0;
+                return frame.getLength();
+            }
+
+            System.arraycopy(readCache, len, readCache, 0, readCacheLength - len);
+            readCacheLength -= len;
+
             return len;
         } catch (IOException e) {
             alive = false;

d48a58b018bda7b0121299c3a1b86774222f0d74
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-06-14 13:37:05 -0700

    release: v0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.benwiegand.projection.geargrinder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

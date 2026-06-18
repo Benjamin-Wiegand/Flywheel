e064683d6a882d8b8cbc18df3c369fe744a6fbe7
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-06-18 15:12:03 -0700

    feat: add permission check when switching operational mode

--- app/src/main/java/io/benwiegand/projection/geargrinder/settings/ui/fragment/MainPreferenceFragment.java
+++ app/src/main/java/io/benwiegand/projection/geargrinder/settings/ui/fragment/MainPreferenceFragment.java
@@ -3,13 +3,28 @@ package io.benwiegand.projection.geargrinder.settings.ui.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import io.benwiegand.projection.geargrinder.R;
+import io.benwiegand.projection.geargrinder.permission.PermissionRequirements;
+import io.benwiegand.projection.geargrinder.settings.PrivilegeMode;
 
 public class MainPreferenceFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences_settings, rootKey);
+
+        ListPreference privilegeModePreference = findPreference(getString(R.string.key_privilege_mode));
+        if (privilegeModePreference != null) privilegeModePreference
+                .setOnPreferenceChangeListener((pref, newValue) -> {
+                    PrivilegeMode newMode = PrivilegeMode.parse(requireContext(), (String) newValue);
+                    switch (newMode) {
+                        case NO_ROOT -> {}
+                        case SHIZUKU -> PermissionRequirements.SHIZUKU_PERMISSION_ENTRY.request().accept(requireActivity());
+                        case ROOT -> PermissionRequirements.ROOT_PERMISSION_ENTRY.request().accept(requireActivity());
+                    }
+                    return true;
+                });
     }
 }

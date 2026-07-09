f7411f143b146e8eefd3dadb19341e46b1a8aa1c
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-07-08 17:39:27 -0700

    fix: enable edge2edge in log recordings activity

--- app/src/main/java/io/benwiegand/projection/geargrinder/LogRecordingsActivity.java
+++ app/src/main/java/io/benwiegand/projection/geargrinder/LogRecordingsActivity.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -53,6 +54,7 @@ public class LogRecordingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_recordings);
+        EdgeToEdge.enable(this);
         setSupportActionBar(findViewById(R.id.action_bar));
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());

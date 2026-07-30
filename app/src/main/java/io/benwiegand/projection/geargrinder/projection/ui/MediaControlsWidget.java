6d9c0b02bcbf9018dadbf259927c3722f03f0dbc
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-07-30 10:35:06 -0700

    fix: track media session priority based on playback state
    the framework appears to have a race condition that sometimes provides the media sessions out of order when switching between media apps.
    
    the only alternative to this that I can think of would be to poll the api, but that's ridiculous.

--- app/src/main/java/io/benwiegand/projection/geargrinder/projection/ui/MediaControlsWidget.java
+++ app/src/main/java/io/benwiegand/projection/geargrinder/projection/ui/MediaControlsWidget.java
@@ -21,9 +21,13 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -36,6 +40,8 @@ import io.benwiegand.projection.geargrinder.thread.ResourceLoaderThread;
 
 public class MediaControlsWidget {
     private static final String TAG = MediaControlsWidget.class.getSimpleName();
+
+    private static final boolean LOG_DEBUG = false;
 
     private final Context context;
     private final View root;
@@ -50,8 +56,7 @@ public class MediaControlsWidget {
     private final ComponentName notificationListenerComponent;
     private final MediaSessionManager mediaSessionManager;
 
-    private MediaController activeSession = null;
-    private boolean playing;
+    private final Deque<SessionWrapper> orderedMediaSessions = new LinkedList<>();
 
     public MediaControlsWidget(View root, Handler handler, ProjectionTaskManager taskManager, Supplier<Optional<PackageService.ServiceBinder>> getPackageBinder) {
         context = root.getContext();
@@ -73,13 +78,27 @@ public class MediaControlsWidget {
         }
 
         root.findViewById(R.id.touch_target).setOnClickListener(v -> openNowPlayingApp());
-        root.findViewById(R.id.skip_prev_button).setOnClickListener(v -> prevTrack());
-        root.findViewById(R.id.play_pause_button).setOnClickListener(v -> playPause());
-        root.findViewById(R.id.skip_next_button).setOnClickListener(v -> nextTrack());
+        root.findViewById(R.id.skip_prev_button).setOnClickListener(v -> {
+            SessionWrapper session = getActiveSession();
+            if (session == null) return;
+            session.skipPrev();
+        });
+        root.findViewById(R.id.play_pause_button).setOnClickListener(v -> {
+            SessionWrapper session = getActiveSession();
+            if (session == null) return;
+            session.playPause();
+        });
+        root.findViewById(R.id.skip_next_button).setOnClickListener(v -> {
+            SessionWrapper session = getActiveSession();
+            if (session == null) return;
+            session.skipNext();
+        });
     }
 
     public void destroy() {
         mediaSessionManager.removeOnActiveSessionsChangedListener(this::onActiveMediaSessionsChanged);
+        for (SessionWrapper session : orderedMediaSessions)
+            session.destroy();
     }
 
     private void hide() {
@@ -90,61 +109,25 @@ public class MediaControlsWidget {
         root.setVisibility(View.VISIBLE);
     }
 
-    private void updatePlaybackState(PlaybackState playbackState) {
+    private void updatePlaybackState(SessionWrapper session) {
         ImageButton skipPrevButton = root.findViewById(R.id.skip_prev_button);
         ImageButton playPauseButton = root.findViewById(R.id.play_pause_button);
         ImageButton skipNextButton = root.findViewById(R.id.skip_next_button);
         View bufferingIndicator = root.findViewById(R.id.buffering_indicator);
         View errorIndicator = root.findViewById(R.id.playback_error_indicator);
-        int state = playbackState != null ? playbackState.getState() : STATE_NONE;
-        switch (state) {
-            case STATE_BUFFERING:
-            case STATE_CONNECTING:
-            case STATE_SKIPPING_TO_PREVIOUS:
-            case STATE_SKIPPING_TO_QUEUE_ITEM:
-            case STATE_SKIPPING_TO_NEXT:
-                playPauseButton.setImageResource(R.drawable.pause);
-                bufferingIndicator.setVisibility(View.VISIBLE);
-                errorIndicator.setVisibility(View.GONE);
-                playing = true;
-                break;
-            case STATE_ERROR:
-                playPauseButton.setImageResource(R.drawable.play_arrow);
-                bufferingIndicator.setVisibility(View.GONE);
-                errorIndicator.setVisibility(View.VISIBLE);
-                playing = false;
-                break;
-            case STATE_PAUSED:
-            case STATE_STOPPED:
-            case STATE_NONE:
-                playPauseButton.setImageResource(R.drawable.play_arrow);
-                bufferingIndicator.setVisibility(View.GONE);
-                errorIndicator.setVisibility(View.GONE);
-                playing = false;
-                break;
-            case STATE_PLAYING:
-            case STATE_FAST_FORWARDING:
-            case STATE_REWINDING:
-                playPauseButton.setImageResource(R.drawable.pause);
-                bufferingIndicator.setVisibility(View.GONE);
-                errorIndicator.setVisibility(View.GONE);
-                playing = true;
-                break;
-            default:
-                Log.wtf(TAG, "unhandled playback state: " + state, new AssertionError());
-                playPauseButton.setImageResource(R.drawable.pause);
-                bufferingIndicator.setVisibility(View.GONE);
-                errorIndicator.setVisibility(View.GONE);
-                playing = true;
-                break;
-        }
 
+        playPauseButton.setImageResource(session.isPlaying() ? R.drawable.pause : R.drawable.play_arrow);
+        bufferingIndicator.setVisibility(session.isLoading() ? View.VISIBLE : View.GONE);
+        errorIndicator.setVisibility(session.isError() ? View.VISIBLE : View.GONE);
+
+        PlaybackState playbackState = session.getPlaybackState();
         long actions = playbackState != null ? playbackState.getActions() : 0;
         skipPrevButton.setVisibility((actions & ACTION_SKIP_TO_PREVIOUS) != 0 ? View.VISIBLE : View.GONE);
         skipNextButton.setVisibility((actions & ACTION_SKIP_TO_NEXT) != 0 ? View.VISIBLE : View.GONE);
     }
 
-    private void updateMetadata(MediaMetadata metadata) {
+    private void updateMetadata(SessionWrapper session) {
+        MediaMetadata metadata = session != null ? session.getMetadata() : null;
         ImageView artImageView = root.findViewById(R.id.media_art_image);
         artImageView.setImageResource(R.drawable.music);
         if (metadata == null) return;
@@ -164,54 +147,77 @@ public class MediaControlsWidget {
     }
 
     private void updateNowPlaying() {
+        SessionWrapper activeSession = getActiveSession();
         if (activeSession == null) {
-            updatePlaybackState(null);
             updateMetadata(null);
             hide();
             return;
         }
-        updatePlaybackState(activeSession.getPlaybackState());
-        updateMetadata(activeSession.getMetadata());
+        updatePlaybackState(activeSession);
+        updateMetadata(activeSession);
         show();
     }
 
-    private final MediaController.Callback activeSessionCallback = new MediaController.Callback() {
-        @Override
-        public void onMetadataChanged(@Nullable MediaMetadata metadata) {
-            updateMetadata(metadata);
-        }
-
-        @Override
-        public void onPlaybackStateChanged(@Nullable PlaybackState state) {
-            updatePlaybackState(state);
-        }
-    };
-
     private void onActiveMediaSessionsChanged(@Nullable List<MediaController> mediaControllers) {
         Log.d(TAG, "media sessions update: " + mediaControllers);
-        MediaController controller = mediaControllers != null && !mediaControllers.isEmpty() ? mediaControllers.get(0) : null;
-        if (activeSession == controller) return;
-        if (activeSession != null)
-            activeSession.unregisterCallback(activeSessionCallback);
-        if (controller != null)
-            controller.registerCallback(activeSessionCallback, handler);
+        if (mediaControllers == null) {
+            try {
+                mediaControllers = mediaSessionManager.getActiveSessions(notificationListenerComponent);
+                Log.d(TAG, "session list: " + mediaControllers);
+            } catch (SecurityException e) {
+                Log.e(TAG, "missing permission to get media sessions: " + e);
+                mediaControllers = List.of();
+            }
+        }
 
-        activeSession = controller;
+        if (LOG_DEBUG) {
+            for (MediaController controller : mediaControllers) {
+                Log.d(TAG, " - " + controller.getPackageName());
+                Log.d(TAG, " --- " + controller.getPlaybackState());
+            }
+        }
+
+        List<SessionWrapper> oldSessions = List.copyOf(orderedMediaSessions);
+        orderedMediaSessions.clear();
+
+        for (MediaController controller : mediaControllers) {
+            MediaSession.Token token = controller.getSessionToken();
+            SessionWrapper session = null;
+            for (SessionWrapper oldSession : oldSessions) {
+                if (!token.equals(oldSession.getToken())) continue;
+                session = oldSession;
+                break;
+            }
+
+            if (session == null) session = new SessionWrapper(controller);
+            orderedMediaSessions.add(session);
+        }
+
         updateNowPlaying();
     }
 
+    private void promoteSessionToActive(SessionWrapper session) {
+        Log.v(TAG, "promoting session to active: " + session);
+        orderedMediaSessions.remove(session);
+        orderedMediaSessions.addFirst(session);
+        updateNowPlaying();
+    }
+
+    private SessionWrapper getActiveSession() {
+        return orderedMediaSessions.peekFirst();
+    }
+
+    private MediaController getActiveMediaController() {
+        SessionWrapper session = getActiveSession();
+        if (session == null) return null;
+        return session.getController();
+    }
+
     private Optional<AppRecord> getActiveMediaApp() {
+        MediaController activeSession = getActiveMediaController();
         if (activeSession == null) return Optional.empty();
         return getPackageBinder.get()
                 .map(b -> b.getApp(activeSession.getPackageName()));
-    }
-
-    private void sendKeyPress(MediaController mediaController, int keyCode) {
-        if (!mediaController.dispatchMediaButtonEvent(new KeyEvent(KeyEvent.ACTION_DOWN, keyCode)))
-            Log.e(TAG, "media key down failed");
-
-        if (!mediaController.dispatchMediaButtonEvent(new KeyEvent(KeyEvent.ACTION_UP, keyCode)))
-            Log.e(TAG, "media key up failed");
     }
 
     private void openNowPlayingApp() {
@@ -219,43 +225,160 @@ public class MediaControlsWidget {
                 .ifPresent(taskManager::dynamicOpen);
     }
 
-    private void prevTrack() {
-        if (activeSession == null) return;
-        if ((activeSession.getFlags() & MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS) != 0) {
-            Log.i(TAG, "using transport controls for skip prev");
-            activeSession.getTransportControls().skipToPrevious();
-            return;
+    private class SessionWrapper extends MediaController.Callback {
+        private final MediaController controller;
+        private int state = STATE_NONE;
+
+        public SessionWrapper(MediaController controller) {
+            this.controller = controller;
+            PlaybackState playbackState = controller.getPlaybackState();
+            if (playbackState != null) state = playbackState.getState();
+
+            controller.registerCallback(this, handler);
         }
 
-        Log.i(TAG, "using media button for skip prev");
-        sendKeyPress(activeSession, KeyEvent.KEYCODE_MEDIA_SKIP_BACKWARD);
-    }
+        public void destroy() {
+            controller.unregisterCallback(this);
+        }
 
-    private void playPause() {
-        if (activeSession == null) return;
-        if ((activeSession.getFlags() & MediaSession.FLAG_HANDLES_MEDIA_BUTTONS) != 0) {
+        public boolean isPlaying() {
+            return switch (state) {
+                case STATE_PLAYING,
+                     STATE_FAST_FORWARDING,
+                     STATE_REWINDING,
+                     STATE_BUFFERING,
+                     STATE_CONNECTING,
+                     STATE_SKIPPING_TO_PREVIOUS,
+                     STATE_SKIPPING_TO_QUEUE_ITEM,
+                     STATE_SKIPPING_TO_NEXT -> true;
+                case STATE_PAUSED,
+                     STATE_STOPPED,
+                     STATE_NONE,
+                     STATE_ERROR -> false;
+                default -> {
+                    Log.wtf(TAG, "unhandled playback state: " + state, new AssertionError());
+                    yield false;
+                }
+            };
+        }
+
+        public boolean isLoading() {
+            return switch (state) {
+                case STATE_BUFFERING,
+                     STATE_CONNECTING,
+                     STATE_SKIPPING_TO_PREVIOUS,
+                     STATE_SKIPPING_TO_QUEUE_ITEM,
+                     STATE_SKIPPING_TO_NEXT -> true;
+                default -> false;
+            };
+        }
+
+        public boolean isError() {
+            return state == STATE_ERROR;
+        }
+
+        public MediaController getController() {
+            return controller;
+        }
+
+        public PlaybackState getPlaybackState() {
+            return controller.getPlaybackState();
+        }
+
+        public MediaMetadata getMetadata() {
+            return controller.getMetadata();
+        }
+
+        public MediaSession.Token getToken() {
+            return controller.getSessionToken();
+        }
+
+        private void sendKeyPress(MediaController mediaController, int keyCode) {
+            if (!mediaController.dispatchMediaButtonEvent(new KeyEvent(KeyEvent.ACTION_DOWN, keyCode)))
+                Log.e(TAG, "media key down failed");
+
+            if (!mediaController.dispatchMediaButtonEvent(new KeyEvent(KeyEvent.ACTION_UP, keyCode)))
+                Log.e(TAG, "media key up failed");
+        }
+
+        public void skipPrev() {
+            if ((controller.getFlags() & MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS) != 0) {
+                Log.i(TAG, "using transport controls for skip prev");
+                controller.getTransportControls().skipToPrevious();
+                return;
+            }
+
+            Log.i(TAG, "using media button for skip prev");
+            sendKeyPress(controller, KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+        }
+
+        public void playPause() {
+            if ((controller.getFlags() & MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS) != 0) {
+                Log.i(TAG, "using transport controls for play/pause");
+                if (isPlaying()) {
+                    controller.getTransportControls().pause();
+                } else {
+                    controller.getTransportControls().play();
+                }
+                return;
+            }
+
             Log.i(TAG, "using media button for play/pause");
-            sendKeyPress(activeSession, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
-            return;
-        }
-        Log.i(TAG, "using transport controls for play/pause");
-        if (playing) {
-            activeSession.getTransportControls().pause();
-        } else {
-            activeSession.getTransportControls().play();
-        }
-    }
-
-    private void nextTrack() {
-        if (activeSession == null) return;
-        if ((activeSession.getFlags() & MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS) != 0) {
-            Log.i(TAG, "using transport controls for skip next");
-            activeSession.getTransportControls().skipToNext();
-            return;
+            sendKeyPress(controller, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
         }
 
-        Log.i(TAG, "using media button for skip next");
-        sendKeyPress(activeSession, KeyEvent.KEYCODE_MEDIA_SKIP_FORWARD);
+        public void skipNext() {
+            if ((controller.getFlags() & MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS) != 0) {
+                Log.i(TAG, "using transport controls for skip next");
+                controller.getTransportControls().skipToNext();
+                return;
+            }
+
+            Log.i(TAG, "using media button for skip next");
+            sendKeyPress(controller, KeyEvent.KEYCODE_MEDIA_NEXT);
+        }
+
+        @Override
+        public void onMetadataChanged(@Nullable MediaMetadata metadata) {
+            if (getActiveSession() == this)
+                updateMetadata(this);
+        }
+
+        @Override
+        public void onPlaybackStateChanged(@Nullable PlaybackState playbackState) {
+            SessionWrapper activeSession = getActiveSession();
+            boolean wasPlaying = isPlaying();
+            state = playbackState != null ? playbackState.getState() : STATE_NONE;
+            boolean playingTransition = isPlaying() && !wasPlaying;
+            boolean playingInactive = isPlaying() && activeSession != null && !activeSession.isPlaying();
+            if (activeSession != this && (playingInactive || playingTransition)) {
+                // this happens due to race conditions in the media session callback api
+                if (playingInactive) Log.d(TAG, "non-active session is playing while active session isn't");
+                if (playingTransition) Log.d(TAG, "non-active session just started playing");
+                promoteSessionToActive(this);
+                return;
+            }
+
+            if (getActiveSession() == this)
+                updatePlaybackState(this);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof SessionWrapper that)) return false;
+            return Objects.equals(controller.getSessionToken(), that.controller.getSessionToken());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(controller.getSessionToken().hashCode());
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return controller.getPackageName() + ": " + getPlaybackState();
+        }
     }
 
 }
